d6c6380d6e6b546a309616ffa40cec1ff7fb60e0
Jerry Walsh <jerry.walsh@gmail.com>
2012-05-12 23:09:03 +0100

    remove meta_search calls

--- app/controllers/spree/admin/uploads_controller.rb
+++ app/controllers/spree/admin/uploads_controller.rb
@@ -9,8 +9,9 @@ class Spree::Admin::UploadsController < Spree::Admin::ResourceController
     def collection
       params[:q] ||= {}
       params[:q][:sort] ||= "created_at.desc"
-      @search = Spree::Upload.search(params[:q])
-      @collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+      #@search = Spree::Upload.search(params[:q])
+      #@collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
+      @collection = Spree::Upload.all
     end
 
 end
