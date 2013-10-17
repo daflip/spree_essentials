19d8c073cfa14b4f5a337db065aea8ae4a1be545
Jerry Walsh <jerry.walsh@gmail.com>
2013-10-17 18:22:54 +0100

    fixes

--- app/controllers/spree/admin/uploads_controller.rb
+++ app/controllers/spree/admin/uploads_controller.rb
@@ -11,7 +11,7 @@ class Spree::Admin::UploadsController < Spree::Admin::ResourceController
       params[:q][:sort] ||= "created_at.desc"
       #@search = Spree::Upload.search(params[:q])
       #@collection = @search.result.page(params[:page]).per(Spree::Config[:orders_per_page])
-      @collection = Spree::Upload.all
+      @collection = Spree::PageImage.all
     end
 
 end
